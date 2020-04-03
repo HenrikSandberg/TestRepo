d7a27294af4f67536ec707a5ee916682c5269f18
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-04-03 14:37:19 +0200

    Create Dag.java

---
+++ Dag.java
@@ -0,0 +1,16 @@
+
+public class Dag {
+    private String navn;
+    private int alder;
+
+    Dog (String navn, int alder) {
+        this.navn = navn;
+        this.alder = alder;
+    }
+
+    @Override
+    public String toString() {
+        return "WOWOW";
+    }
+
+}
