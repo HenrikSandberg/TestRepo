ce78af021d5748355e8ee066e978568c32dd71ce
MathiasBOSS <Mathias@aek.no>
2020-04-03 14:27:13 +0200

    Added new person "Mathias"

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Person a = Person("Henrik", 28);
+        Person a = new Person("Henrik", 28);
+        Person b = new Person("Mathias", 19);
     }
 }
