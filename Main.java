b59b99d019c489b35e7ba4faebaf2e830c09e6ac
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2020-04-03 14:38:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Person a = new Person("Henrik", 28);
-        String print = a.toString();
-        System.out.println(print);
-
+        System.out.println(new Person("Henrik", 28).toString());
         Person b = new Person("Mathias", 19);
         System.out.println(b.toString());
     }
